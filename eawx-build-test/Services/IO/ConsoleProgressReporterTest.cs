0f7fafcd51da96b5ced3ca88aea1322350c7f0f5
Sven Marcus <s.marcus@outlook.de>
2021-02-03 23:38:00 +0100

    Begin ConsoleTaskProgressReporter

---
+++ eawx-build-test/Services/IO/ConsoleProgressReporterTest.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.IO;
+using System.Text;
+using EawXBuild.Core;
+using EawXBuild.Services.IO;
+using EawXBuildTest.Core;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace EawXBuildTest.Services.IO {
+    [TestClass]
+    public class ConsoleProgressReporterTest {
+        private StringBuilder _stringBuilder;
+        private TextWriter _out;
+
+        [TestInitialize]
+        public void SetUp() {
+            _stringBuilder = new StringBuilder();
+            _out = Console.Out;
+            Console.SetOut(new StringWriter(_stringBuilder));
+        }
+
+        [TestMethod]
+        public void WhenReportingTaskStart__ShouldPrintTaskStartToConsole() {
+            const string expectedTaskId = "taskId";
+            const string expectedTaskName = "My Task";
+
+            var progress = new TaskProgress();
+            var sut = new ConsoleTaskProgressReporter(progress);
+
+            var task = new TaskDummy {
+                Id = expectedTaskId,
+                Name = expectedTaskName
+            };
+
+            progress.Report(TaskStatus.Started, task);
+
+
+            var actual = _stringBuilder.ToString().Trim();
+            var expected = $"[Start     ] {nameof(TaskDummy)} - Id: {expectedTaskId} - Name: {expectedTaskName}";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void WhenReportingTaskFinish__ShouldPrintTaskFinishToConsole() {
+            const string expectedTaskId = "taskId";
+            const string expectedTaskName = "My Task";
+
+            var progress = new TaskProgress();
+            var sut = new ConsoleTaskProgressReporter(progress);
+
+            var task = new TaskDummy {
+                Id = expectedTaskId,
+                Name = expectedTaskName
+            };
+
+            progress.Report(TaskStatus.Finished, task);
+
+
+            var actual = _stringBuilder.ToString().Trim();
+            var expected = $"[       End] {nameof(TaskDummy)} - Id: {expectedTaskId} - Name: {expectedTaskName}";
+            Assert.AreEqual(expected, actual);
+        }
+
+        [TestMethod]
+        public void WhenReportingTaskStartAndFinish__ShouldPrintTaskStartAndFinishToConsole() {
+            const string expectedTaskId = "taskId";
+            const string expectedTaskName = "My Task";
+
+            var progress = new TaskProgress();
+            var sut = new ConsoleTaskProgressReporter(progress);
+
+            var task = new TaskDummy {
+                Id = expectedTaskId,
+                Name = expectedTaskName
+            };
+
+            progress.Report(TaskStatus.Started, task);
+            progress.Report(TaskStatus.Finished, task);
+
+
+            var actual = _stringBuilder.ToString().Trim();
+            var taskDetails = $"{nameof(TaskDummy)} - Id: {expectedTaskId} - Name: {expectedTaskName}";
+            var expected = $"[Start     ] {taskDetails}" + Environment.NewLine + $"[       End] {taskDetails}";
+            Console.SetOut(_out);
+            Console.Write(actual);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
---
+++ eawx-build/Services/IO/ConsoleTaskProgressReporter.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using EawXBuild.Core;
+
+namespace EawXBuild.Services.IO {
+    public class ConsoleTaskProgressReporter {
+
+        public ConsoleTaskProgressReporter(TaskProgress progress) {
+            progress.OnReport += (sender, args) => {
+                switch (args.Status) {
+                    case TaskStatus.Started:
+                        OnStartedReport(args);
+                        break;
+                    case TaskStatus.Finished:
+                        OnFinishedReport(args);
+                        break;
+                    case TaskStatus.Failed:
+                        break;
+                    default:
+                        throw new ArgumentOutOfRangeException();
+                };
+            };
+        }
+
+        private static void OnStartedReport(TaskProgressEventArgs args) {
+            Console.Write("[");
+            Console.ForegroundColor = ConsoleColor.Blue;
+            Console.Write("Start");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write($"     ] {args.Task.GetType().Name} - Id: {args.Task.Id} - Name: {args.Task.Name}" + System.Environment.NewLine);
+        }
+
+        private static void OnFinishedReport(TaskProgressEventArgs args) {
+            Console.Write("[       ");
+            Console.ForegroundColor = ConsoleColor.Green;
+            Console.Write("End");
+            Console.ForegroundColor = ConsoleColor.Black;
+            Console.Write($"] {args.Task.GetType().Name} - Id: {args.Task.Id} - Name: {args.Task.Name}" + System.Environment.NewLine);
+        }
+    }
+}
